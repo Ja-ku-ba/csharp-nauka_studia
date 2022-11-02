5a07a2bbd863cdbbe05100ea306b71c9adf3dae4
wul-fryk <94911788+wul-fryk@users.noreply.github.com>
2022-11-02 21:08:11 +0100

    Create kalkulator.cs

---
+++ kalkulator.cs
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main(string[] args)
+  {
+     Console.Write("Podaj pierwszą liczbę: ");
+     double first = Convert.ToDouble(Console.ReadLine());
+     Console.Write("Podaj operator (+, -, *, /): ");
+     string op = Console.ReadLine();
+     Console.Write("Podaj drugą liczbę: ");
+     double sec = Convert.ToDouble(Console.ReadLine());
+     
+     if (op == "+"){
+         Console.WriteLine(first + sec);
+     }
+     else if (op == "-"){
+         Console.WriteLine(first - sec);
+     }
+     else if (op == "*"){
+         Console.WriteLine(first * sec);
+     }
+     else if (op == "/"){
+         Console.WriteLine(first / sec);
+     }
+     else{
+         Console.WriteLine("Wprowadzono nieprawidłowy operator");
+     } 
+  }
+}
